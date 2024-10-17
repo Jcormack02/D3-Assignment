66a0e17cd48e88eb5b3f2250d379169af3bd8377
Jcormack02 <2413552@dundee.ac.uk>
2024-10-17 18:48:11 +0100

    added in button functionality to allow changes in number of towns, also added the scaling of the dot size in relation to population (the default number of towns on refresh does not work yet)

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-function setupMap() {
+function setupMap(numTowns = 50) { //default number of towns being 50 if no number is chosen by the user
     // Use ArcGIS's AMD module loader to load Esri modules
     require([
-        "esri/Map", // To obtain the map template
-        "esri/views/MapView", // To get the specific version of the map
+        "esri/Map",
+        "esri/views/MapView",
         "esri/Graphic",  // To plot the points on the map
         "esri/layers/GraphicsLayer"  // To hold all the points
     ], function(Map, MapView, Graphic, GraphicsLayer) {
@@ -24,8 +24,13 @@ function setupMap() {
         const graphicsLayer = new GraphicsLayer();
         map.add(graphicsLayer);
 
+        // Create a scale for the size of the point in relation to population
+        const sizeScale = d3.scaleLinear()
+        .domain([10000, 500000]) // The population range
+        .range([4, 70]) // The size difference (4px-20px)
+
         // Fetch data using D3 and add it to the map
-        d3.json("http://34.147.162.172/Circles/Towns/50")
+        d3.json(`http://34.147.162.172/Circles/Towns/${numTowns}`)
           .then(function(data) {
             console.log("Data fetched:", data);
 
@@ -41,8 +46,12 @@ function setupMap() {
                     geometry: point,
                     symbol: {
                         type: "simple-marker",
-                        color: "red",
-                        size: "8px"
+                        color: "rgba(255, 0, 0, 0.6)", //This makes the red slightly opaque
+                        size: sizeScale(location.Population), // Setting the size of the dot in relation to the population size
+                        outline: {
+                            color: "rgba(0, 0, 0, 0.3)",  // Optional: add a semi-transparent outline
+                            width: 1
+                        }
                     },
                     attributes: location,
                     popupTemplate: {
@@ -59,3 +68,20 @@ function setupMap() {
           });
     });
 }
+
+
+function updateMap() {
+    // Get the user input for the number of towns
+    const numTowns = document.getElementById("quantity").value;
+
+    // Clear the current map (you can implement a method to clear the previous data if needed)
+    document.getElementById("viewDiv").innerHTML = "";  // This will reset the map view div
+
+    // Call setupMap with the user input, or default to 50 if input is invalid
+    setupMap(numTowns);
+}
+
+// Call setupMap() with 50 as default on page load
+window.onload = function() {
+    setupMap(50);  // Display 50 towns by default
+};
